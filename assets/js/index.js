21fbfa289f616b5bd343cdadd20d36c00919ba6a
Naveen Gupta <ernaveengupta@rediffmail.com>
2013-08-20 12:38:03 +0530

    updated url to DGRReport.ashx

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,8 +24,8 @@ function onDeviceReady() {
             } else {
                 window.localStorage.clear();
             }
-            
-            $.ajax({ url: 'http://192.168.1.57/OEGServer/Classes/Handler.ashx',
+
+            $.ajax({ url: 'http://192.168.1.57/OEGServer/Classes/DGRReport.ashx',
                 data: { username: email, password: pwd }, // Convert a form to a JSON string representation
                 dataType: 'text',
                 type: 'POST',
@@ -56,7 +56,9 @@ function onDeviceReady() {
                 },
                 error: function (request, error) {
                      // This callback function will trigger on unsuccessful action                
-                   alert('Network error has occurred please try again!, readyState:' + request.readyState + " status:" + request.status + " statusText:" + request.statusText + " response:" + request.responseText);
+                   //alert('Network error has occurred please try again!, readyState:' + request.readyState + " status:" + request.status + " statusText:" + request.statusText + " response:" + request.responseText);
+                    alert("Network error has occurred please try again!");
+
                   }
             });
         } else {
@@ -73,79 +75,79 @@ $(document).on('pagebeforeshow', '#login', function () {
     
 });
 
-$(document).on('pagebeforeshow', '#second', function () {
-    $('#second [data-role="content"]').html(resultObject.formSubmitionResult);
-  
-    var switched = false;
-    var updateTables = function () {
-        if (($(window).width() < 767) && !switched) {
-            switched = true;
-            $("table.responsive").each(function (i, element) {
-                splitTable($(element));
-            });
-            return true;
-        }
-        else if (switched && ($(window).width() > 767)) {
-            switched = false;
-            $("table.responsive").each(function (i, element) {
-                unsplitTable($(element));
-            });
-        }
-    };
 
-    updateTables();
-    $(window).on("redraw", function () { switched = false; updateTables(); }); // An event to listen for
-    $(window).on("resize", updateTables);
-
-
-
-    function splitTable(original) {
-        original.wrap("<div class='table-wrapper' />");
-
-        var copy = original.clone();
-        copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
-        copy.removeClass("responsive");
-
-        original.closest(".table-wrapper").append(copy);
-        copy.wrap("<div class='pinned' />");
-        original.wrap("<div class='scrollable' />");
-
-        setCellHeights(original, copy);
-    }
-
-    function unsplitTable(original) {
-        original.closest(".table-wrapper").find(".pinned").remove();
-        original.unwrap();
-        original.unwrap();
-    }
-
-    function setCellHeights(original, copy) {
-        var tr = original.find('tr'),
-        tr_copy = copy.find('tr'),
-        heights = [];
-
-        tr.each(function (index) {
-            var self = $(this),
-          tx = self.find('th, td');
-
-            tx.each(function () {
-                var height = $(this).outerHeight(true);
-                heights[index] = heights[index] || 0;
-                if (height > heights[index]) heights[index] = height;
-            });
-
-        });
-
-        tr_copy.each(function (index) {
-            $(this).height(heights[index]);
-        });
-    }
-
-
-});
 
 var resultObject = {
     formSubmitionResult : null
 }
 
 
+var switched = false;
+var updateTables = function () {
+    if (($(window).width() < 767) && !switched) {
+        switched = true;
+        $("table.responsive").each(function (i, element) {
+            splitTable($(element));
+        });
+        return true;
+    }
+    else if (switched && ($(window).width() > 767)) {
+        switched = false;
+        $("table.responsive").each(function (i, element) {
+            unsplitTable($(element));
+        });
+    }
+};
+
+
+function splitTable(original) {
+    original.wrap("<div class='table-wrapper' />");
+
+    var copy = original.clone();
+    copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
+    copy.removeClass("responsive");
+
+    original.closest(".table-wrapper").append(copy);
+    copy.wrap("<div class='pinned' />");
+    original.wrap("<div class='scrollable' />");
+
+    setCellHeights(original, copy);
+}
+
+function unsplitTable(original) {
+    original.closest(".table-wrapper").find(".pinned").remove();
+    original.unwrap();
+    original.unwrap();
+}
+
+function setCellHeights(original, copy) {
+    var tr = original.find('tr'),
+        tr_copy = copy.find('tr'),
+        heights = [];
+
+    tr.each(function (index) {
+        var self = $(this),
+          tx = self.find('th, td');
+
+        tx.each(function () {
+            var height = $(this).outerHeight(true);
+            heights[index] = heights[index] || 0;
+            if (height > heights[index]) heights[index] = height;
+        });
+
+    });
+
+    tr_copy.each(function (index) {
+        $(this).height(heights[index]);
+    });
+}
+
+$(document).on('pagebeforeshow', '#second', function () {
+    $('#second [data-role="content"]').html(resultObject.formSubmitionResult);
+    switched = false;
+    updateTables();
+    //$(window).on("redraw", function () { switched = false; updateTables(); }); // An event to listen for
+    //$(window).on("resize", updateTables);
+
+
+});
